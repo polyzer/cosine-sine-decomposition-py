8ecca4a9a615f7c6dcaf454d2944ed0edf74fb16
polyzer <realpolyzer@yandex.ru>
2021-05-14 21:08:30 +0500

    update

---
+++ csd.py
@@ -0,0 +1,11 @@
+import numpy as np
+from typing import 
+
+
+def csd(u):
+    """
+    The complete CS decomposition (CSD) applies to any m-by-m matrix X from the
+    unitary group U(m), viewed as a 2-by-2 block matrix.
+    Brian D. Sutton - Computing the complete CS decomposition https://arxiv.org/abs/0707.1838
+    """
+    pass
